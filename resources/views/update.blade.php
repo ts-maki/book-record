a32b3bbc6e28ca58d3df5f84977024ff0bd59e9e
hare <washi79washi@gmail.com>
2023-05-08 20:24:11 +0900

    編集画面のradioボタンをfor文で展開

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -7,32 +7,22 @@
             <p>{{ $record->book->title }}</p>
             <p>{{ $record->book->author }}</p>
             <p>{{ $record->read_date }}</p>
+            <p>{{ $record->category->name }}</p>
         </div>
     </div>
     <form action="{{ route('record.update', $record->id) }}" method="post">
         @method('PUT')
         @csrf
-        <label><input type="radio" name="category_id" value="1" {{ old('category_id', $record->category_id) ===
-            1 ? "checked" : ""}}>ファンタジー</label>
-        <label><input type="radio" name="category_id" value="2" {{ old('category_id', $record->category_id) ===
-            2 ? "checked" : ""}}>恋愛</label>
-        <label><input type="radio" name="category_id" value="3" {{ old('category_id', $record->category_id) ===
-            3 ? "checked" : ""}}>青春</label>
-        <label><input type="radio" name="category_id" value="4" {{ old('category_id', $record->category_id) ===
-            4 ? "checked" : ""}}>SF</label>
-        <label><input type="radio" name="category_id" value="5" {{ old('category_id', $record->category_id) ===
-            5 ? "checked" : ""}}>ミステリー</label>
-        <label><input type="radio" name="category_id" value="6" {{ old('category_id', $record->category_id) ===
-            6 ? "checked" : ""}}>ホラー</label>
-        <label><input type="radio" name="category_id" value="7" {{ old('category_id', $record->category_id) ===
-            7 ? "checked" : ""}}>仕事</label>
-        <label><input type="radio" name="category_id" value="8" {{ old('category_id', $record->category_id) ===
-            8 ? "checked" : ""}}>歴史</label>
-        <h4>感想</h4>
-        <div>
-            <input type="date" name="read_date" value="{{ old('read_date', $record->read_date) }}">
-        </div>
-        <textarea name="content" cols="30" rows="10">{{ old('content', $record->content) }}</textarea>
-        <input type="submit" value="編集">
+        @for ($i = 1; $i <= $record->category->count(); $i++)
+            <label><input type="radio" name="category_id" value="{{ $i }}" {{ old('category_id', $record->category_id)
+                ===
+                $i ? "checked" : ""}}>{{ $record->category->find($i)->name }}</label>
+            @endfor
+            <h4>感想</h4>
+            <div>
+                <input type="date" name="read_date" value="{{ old('read_date', $record->read_date) }}">
+            </div>
+            <textarea name="content" cols="30" rows="10">{{ old('content', $record->content) }}</textarea>
+            <input type="submit" value="編集">
     </form>
 </x-layout>
