79f4129e1e05b5acc2790a99f3458f2da2d0d4aa
hare <washi79washi@gmail.com>
2023-05-08 10:35:01 +0900

    感想一覧にタイトルと感想を表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,13 @@
     <h1></h1>
     <h1>登録した感想</h1>
     @foreach ($records as $record)
-    <img src="{{ $record->book->thumbnail_path }}">
+    <div class="flex">
+        <img src="{{ $record->book->thumbnail_path }}">
+        <div>
+            <h4>{{ $record->book->title }}</h4>
+            <hr>
+            <p>{{ $record->content }}</p>
+        </div>
+    </div>
     @endforeach
 </x-layout>
