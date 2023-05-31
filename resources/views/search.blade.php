904799b00d3fbf3c00d6fb860b73cb4f95905454
hare <washi79washi@gmail.com>
2023-05-31 20:43:01 +0900

    虫眼鏡のスタイル

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,12 +3,12 @@
         <h2>本の検索</h2>
         <form action="{{ route('book.search') }}" method="post">
             @csrf
-            <input type="search" name="serach_keyword" placeholder="キーワードを入力してください" class="w-1/2 rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring2" />
-            <button type="submit" value="検索"><img src="{{ asset('storage/images/search_black_24dp.svg') }}"></button>
+            <input type="search" name="serach_keyword" placeholder="キーワードを入力してください"
+                class="w-1/2 rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring2" />
+            <button type="submit" value="検索" class="-translate-x-8 translate-y-2 scale-150"><img
+                    src="{{ asset('storage/images/search_black_24dp.svg') }}"></button>
         </form>
         @if (!empty($books))
-        <h2>検索結果</h2>
-
         @foreach ($books as $book)
         <div class="flex rounded-lg border sm:flex-row mt-6 bg-white drop-shadow-md">
             <img src="{{ $book['thumbnail_path'] }}" class="rounded-l-lg">
@@ -32,7 +32,8 @@
                             <input type="hidden" name="thumbnail_path" value="{{ $book['thumbnail_path'] }}">
                             <input type="hidden" name="isbn" value="{{ $book['isbn'] }}">
                             <input type="hidden" name="published_date" value="{{ $book['published_date'] }}">
-                            <input type="submit" value="登録" class="px-2 hover:border-green-200 hover:border-solid hover:border-2 hover:bg-white rounded-full border-2 border-green-300 text-white bg-green-300 duration-300 hover:text-green-300">
+                            <input type="submit" value="登録"
+                                class="px-2 hover:border-green-200 hover:border-solid hover:border-2 hover:bg-white rounded-full border-2 border-green-300 text-white bg-green-300 duration-300 hover:text-green-300">
                         </form>
                     </div>
                 </div>
