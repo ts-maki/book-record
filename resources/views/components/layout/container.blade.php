90c6973cbc4442d72d123e5c5ba7684fd177a929
hare <washi79washi@gmail.com>
2023-06-04 14:32:50 +0900

    style(blade): 感想登録画面のtailwindCSS調整

--- resources/views/components/layout/container.blade.php
+++ resources/views/components/layout/container.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full py-6 ">
-    <div {{ $attributes->merge(['class' => 'container mx-auto max-w-4xl px-4']) }}>
+    <div {{ $attributes->merge(['class' => 'container mx-auto max-w-[720px] px-4']) }}>
         {{ $slot }}
     </div>
 </div>
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,31 +1,36 @@
 <x-layout>
-    <x-layout.container class="bg-white">
-        <h1>感想を登録する</h1>
-        <div class="flex py-4">
-            <img src="{{ $book->thumbnail_path }}">
-            <div class="px-4">
-                <p>タイトル: {{ $book->title }}</p>
-                <p>著者: {{ $book->author }}</p>
+    <div class="px-4">
+        <x-layout.container class="bg-white rounded-lg py-4">
+            <h3 class="pb-2 border-b-2 border-green-200">感想を登録する</h3>
+            <div class="flex pt-2">
+                <img src="{{ $book->thumbnail_path }}">
+                <div class="pl-2">
+                    <p class="text-2xl">{{ $book->title }}</p>
+                    <p>{{ $book->author }}</p>
+                </div>
             </div>
-        </div>
-        <h3>近しいカテゴリーを選んでください</h3>
-        <form action="{{ route('book.record', $book->id) }}" method="post" class="book__record-category pt-4">
-            @csrf
-            <label><input type="radio" name="category" value="ファンタジー">ファンタジー</label>
-            <label><input type="radio" name="category" value="恋愛">恋愛</label>
-            <label><input type="radio" name="category" value="青春">青春</label>
-            <label><input type="radio" name="category" value="SF">SF</label>
-            <label><input type="radio" name="category" value="ミステリー">ミステリー</label>
-            <label><input type="radio" name="category" value="ホラー">ホラー</label>
-            <label><input type="radio" name="category" value="仕事">仕事</label>
-            <label><input type="radio" name="category" value="歴史">歴史</label>
-            <p class="pt-6">読了日</p>
-            <div class="pt-2">
-                <input type="date" name="date" value="<?php echo date('Y-m-d') ?>">
-            </div>
-            <p class="pt-4">感想</p>
-            <textarea name="content" cols="30" rows="10" class="mt-2"></textarea>
-            <input type="submit" value="登録" class="block px-2 hover:border-green-200 hover:border-solid hover:border-2 hover:bg-white rounded-full border-2 border-green-300 text-white bg-green-300 duration-300 hover:text-green-300">
-        </form>
-    </x-layout.container>
+            <h3>近しいカテゴリーを選んでください</h3>
+            <form action="{{ route('book.record', $book->id) }}" method="post" class="book__record-category pt-4">
+                @csrf
+                <label><input type="radio" name="category" value="ファンタジー" class="border-gray-300"><span class="pl-1">ファンタジー</span></label>
+                <label><input type="radio" name="category" value="恋愛" class="border-gray-300"><span class="pl-1">恋愛</span></label>
+                <label><input type="radio" name="category" value="青春" class="border-gray-300"><span class="pl-1">青春</span></label>
+                <label><input type="radio" name="category" value="SF" class="border-gray-300"><span class="pl-1">SF</span></label>
+                <label><input type="radio" name="category" value="ミステリー" class="border-gray-300"><span class="pl-1">ミステリー</span></label>
+                <label><input type="radio" name="category" value="ホラー" class="border-gray-300"><span class="pl-1">ホラー</span></label>
+                <label><input type="radio" name="category" value="仕事" class="border-gray-300"><span class="pl-1">仕事</span></label>
+                <label><input type="radio" name="category" value="歴史" class="border-gray-300"><span class="pl-1">歴史</span></label>
+                <div class="sm:flex pt-6">
+                    <p class="w-1/3 pb-2">読了日</p>
+                    <input type="date" name="date" value="<?php echo date('Y-m-d') ?>" class="border-gray-300 rounded-lg">
+                </div>
+                <div class="sm:flex">
+                    <p class="pt-4 sm:w-1/3">感想</p>
+                    <textarea name="content" cols="30" rows="3" class="mt-2 sm:w-2/3 w-full border-gray-300 rounded-lg"></textarea>
+                </div>
+                <input type="submit" value="登録"
+                    class="block px-2 hover:border-green-200 hover:border-solid hover:border-2 hover:bg-white rounded-full border-2 border-green-300 text-white bg-green-300 duration-300 hover:text-green-300 mx-auto mt-4">
+            </form>
+        </x-layout.container>
+    </div>
 </x-layout>
