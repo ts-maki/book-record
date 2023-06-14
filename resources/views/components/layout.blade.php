3bc3b2a79dd0ed70e49d16976f6e17023bc0aa98
hare <washi79washi@gmail.com>
2023-06-14 20:59:11 +0900

    style: フォントをメイリオに変更

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite('resources/css/app.css')
     <title>本の感想共有アプリ</title>
 </head>
 
 <body class="h-full bg-emerald-50">
     <x-layout.header></x-layout.header>
-    
+
     {{ $slot }}
 </body>
 
