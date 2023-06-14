a3c3bc40668aceaa6c8aebe8b952ac360e72d2a6
hare <washi79washi@gmail.com>
2023-06-14 12:16:57 +0900

    style: プロフィール編集の余白調整

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,19 +7,17 @@
     <x-element.breadcrumbs>
         {{ Breadcrumbs::render('profile.edit') }}
     </x-element.breadcrumbs>
-    <x-layout.container>
+    <x-layout.container class="space-y-6">
         <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.update-profile-information-form')
             </div>
         </div>
-
         <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.update-password-form')
             </div>
         </div>
-
         <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.delete-user-form')
