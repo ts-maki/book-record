f20316bc6bbd3e18cf18da4d64d1521dd549916b
hare <washi79washi@gmail.com>
2023-06-24 14:48:06 +0900

    feat: 本検索時の未入力時のバリデーション処理

---
+++ app/Http/Requests/SearchPostRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'serach_keyword' => 'required',
+        ];
+    }
+
+    /**
+     * 定義済みバリデーションルールのエラーメッセージ取得
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'serach_keyword.required' => '本のタイトルか著者名を入力してね！',
+        ];
+    }
+}
