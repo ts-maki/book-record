43b599009dec83da3382d40956af3a1c4c4868a8
hare <washi79washi@gmail.com>
2023-07-04 22:54:19 +0900

    feat: 404エラー画面途中

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
 @section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('code', '404クライアントエラーレスポンス')
+@section('message', '存在しないページです')
