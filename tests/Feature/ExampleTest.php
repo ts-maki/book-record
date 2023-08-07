59e5ebd881d152a0adcb56f60285041b64462231
hare <washi79washi@gmail.com>
2023-08-07 21:33:03 +0900

    test: 作成したテストユーザーがホーム画面にアクセスするテスト成功

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,10 +12,32 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_sample1(): void
     {
         $response = $this->get('/');
 
+        $response->assertStatus(200);
+    }
+
+    public function test_sample2(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertStatus(200);
+    }
+
+    function test_aaa()
+    {
+        // Arrange（準備）
+        $user = User::factory()->create();
+
+        // Act（実行）
+        $response = $this
+            ->actingAs($user)
+            ->get('/');
+
+        // Assert（検証）
         $response->assertStatus(200);
     }
 }
