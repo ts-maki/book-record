5af2fa2ba2b1aa83596a0191e97f4ed701c704a5
hare <washi79washi@gmail.com>
2023-08-15 12:18:22 +0900

    test: 感想一覧と認証済み指定ユーザーの感想一覧のテスト

---
+++ tests/Feature/HTTP/Controllers/ListControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature\HTTP\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+class ListControllerTest extends TestCase
+{
+    /**
+     * 感想一覧ページ
+     */
+    public function test_感想一覧(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * 自分の感想一覧
+     */
+    public function test_自分の感想一覧(): void
+    {
+        $user = User::factory()->create([
+            'name' => 'テストユーザー',
+            'email' => 'testdtsestw',
+            'password' => Str::random(60)
+        ]);
+
+        //認証されていなっかたら302リダイレクト
+        $response = $this->get('user/my/' . $user->id);
+        $response->assertStatus(302);
+
+        $this->actingAs($user);
+        $response = $this->get('user/my/' . $user->id);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * 自分のお気に入り一覧
+     */
+    public function test_自分のお気に入り一覧(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
