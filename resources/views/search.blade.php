48b5e8613db52cd551b07a5ae0f5eccd48360528
hare <washi79washi@gmail.com>
2023-09-13 16:36:28 +0900

    fix : 元に戻す

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -14,10 +14,7 @@
         </form>
         <x-input-error class="mt-2" :messages="$errors->get('search_keyword')" />
         @if (!empty($books))
-        @if (mb_strlen($books) >= 12)
-        <p class="mt-2 text-red-500">APIリクエスト超過です。すみませんが翌日ご利用ください</p>
-        @else
-        (is_string($books))
+        @if  (is_string($books) !== false)
         <p class="mt-2 text-red-500">検索結果が0件です</p>
         @endif
         @if (is_array($books))
