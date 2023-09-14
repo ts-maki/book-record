e0534ab3c861b6d4bb63428251cac1e9c21f358c
hare <washi79washi@gmail.com>
2023-09-14 12:59:43 +0900

    test: test_自分の感想から感想編集実装

--- tests/Feature/HTTP/Controllers/EditRecordControllerTest.php
+++ tests/Feature/HTTP/Controllers/EditRecordControllerTest.php
@@ -29,4 +29,22 @@ class EditRecordControllerTest extends TestCase
         $response->assertRedirect(route('index'));
     }
 
+    public function test_自分の感想から感想編集(): void
+    {
+        $login_user_id = User::find(3);
+        $this->actingAs($login_user_id);
+        $response = $this->get('user/my/3/edit/15');
+        $response->assertStatus(200);
+
+        $updateData = [
+            'book_id' => 298,
+            'user_Id' => 3,
+            'category_id' => fake()->numberBetween(1, 8),
+            'content' => fake()->realText(30),
+            'read_date' => fake()->date("Y-m-d")
+        ];
+
+        $response = $this->put(route('user.record.update', ['user_id' => 3,'record_id' => 15]), $updateData);
+        $response->assertRedirect(route('my.record', ['user_id' => 3]));
+    }
 }
