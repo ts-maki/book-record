2141331350444dd5360f9dfa8ee542a68511bb0f
t.sakamaki <t.sakamaki@promptk.jp>
2023-09-26 09:05:21 +0900

    fix: ログイン保持を戻す

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,6 +32,16 @@
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox"
+                            class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                            name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+
                 <div class="flex items-center justify-between mt-4">
                     <x-element.auth-button-link :href="route('index')">ゲストでログインする</x-element.auth-button-link>
                     <x-primary-button class="ml-3">
